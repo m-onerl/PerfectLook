844f44e293772c331a6729c2b4f442b4735d6c9d
DoublexD <dominikduma40@gmail.com>
2024-11-24 22:26:29 +0100

    connect to database

--- dbConnect.js
+++ dbConnect.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "meble",
+  database: "clothes",
 });
 
 connection.connect((err) => {
